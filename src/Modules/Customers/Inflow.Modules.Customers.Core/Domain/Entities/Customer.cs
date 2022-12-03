e08921ef3216d2a959066b04ac424ca1047f38f3
Vinicius Mabellini <vpmabellini@gmail.com>
2022-12-03 20:28:04 -0300

    Customer entity v1

---
+++ src/Modules/Customers/Inflow.Modules.Customers.Core/Domain/Entities/Customer.cs
@@ -0,0 +1,36 @@
+﻿using Inflow.Shared.Abstractions.Kernel.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inflow.Modules.Customers.Core.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; private set; }
+        public Email Email { get; private set; }
+        public string Name { get; private set; }
+        public string FullName { get; private set; }
+        public string Address { get; private set; }
+        public string Natiolatity { get; private set; }
+        public string Identity { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? CompletedAt { get; private set; }
+        public DateTime? VerifiedAt { get; private set; }
+
+        private Customer()
+        {
+        }
+
+        public Customer(Guid id, Email email, DateTime createdAt)
+        {
+            Id = id;
+            Email = email;
+            CreatedAt = createdAt;
+        }
+
+    }
+}

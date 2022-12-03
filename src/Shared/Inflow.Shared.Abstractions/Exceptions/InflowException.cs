30a0ce73fe6d3a819824861c9567581da857b330
Vinicius Mabellini <vpmabellini@gmail.com>
2022-12-03 20:23:18 -0300

    Base exception and email valueobject

---
+++ src/Shared/Inflow.Shared.Abstractions/Exceptions/InflowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inflow.Shared.Abstractions.Exceptions
+{
+    public abstract class InflowException : Exception
+    {
+        protected InflowException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ src/Shared/Inflow.Shared.Abstractions/Kernel/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,19 @@
+﻿using Inflow.Shared.Abstractions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inflow.Shared.Abstractions.Kernel.Exceptions
+{
+    public class InvalidEmailException : InflowException
+    {
+        public InvalidEmailException(string email) : base($"Email {email} is invalid")
+        {
+            Email = email;
+        }
+
+        public string Email { get; }
+    }
+}
